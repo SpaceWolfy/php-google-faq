e74add143538ee691532068f924c8ac9d662640d
Samuel Scozzari <s_scozzari@libero.it>
2022-02-16 14:37:54 +0100

    html structure modified

--- index.php
+++ index.php
@@ -129,14 +129,6 @@
   ];
 
   /* var_dump($db); */
-
-
-  foreach($db as $faq => $answer) {
-    echo '<'. $answer['titleSize'] .'>' .$faq . '</'. $answer['titleSize'] .'>';
-    foreach($answer['paragraphs'] as $p => $value) {
-      echo '<p>' . $value . '</p>';
-    }
-  }
 ?>
 
 <!DOCTYPE html>
@@ -145,9 +137,26 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Domande frequenti - Privacy e termini</title>
+  
+  <link rel="stylesheet" href="style.css" />
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-  
+  <header></header>
+  <main>
+    <?php 
+      foreach($db as $faq => $answer) {
+        echo '<'. $answer['titleSize'] .'>' .$faq . '</'. $answer['titleSize'] .'>';
+        foreach($answer['paragraphs'] as $p => $value) {
+          echo '<p>' . $value . '</p>';
+        }
+      }
+    ?>
+  </main>
+  <footer></footer>
 </body>
 </html>
